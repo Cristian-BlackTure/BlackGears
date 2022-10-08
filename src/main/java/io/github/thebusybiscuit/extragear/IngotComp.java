c9d8e16356c6b8aafa1da6f27a5c4f870a0b5b07
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 20:57:23 +0300

    Create IngotComp.java

--- src/main/java/io/github/thebusybiscuit/extragear/IngotComp.java
+++ src/main/java/io/github/thebusybiscuit/extragear/IngotComp.java
@@ -1,37 +1,101 @@
 package io.github.thebusybiscuit.extragear;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-import org.bstats.bukkit.Metrics;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
+import javax.annotation.ParametersAreNonnullByDefault;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import io.github.bakedlibs.dough.items.CustomItemStack;
+import io.github.bakedlibs.dough.protection.Interaction;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
+
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
+import me.mrCookieSlime.Slimefun.api.inventory.DirtyChestMenu;
+import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
 
-public class IngotComp extends AContainer implements RecipeDisplayItem {
+public class IngotComp extends AContainer {
+
+    @ParametersAreNonnullByDefault
     public IngotComp(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
-     super(itemGroup, item, recipeType, recipe);}
-        @Override
-        protected void registerDefaultRecipes() {
+        super(itemGroup, item, recipeType, recipe);
 
-            registerRecipe(8, new ItemStack[] {new ItemStack(Material.COBBLESTONE, 8)}, new ItemStack[] {Material.COBBLESTONE, 1});
-            registerRecipe(16, new SlimefunItemStack[]{new SlimefunItemStack(Material.COBBLESTONE, 8)}, new ItemStack[] {Material.COBBLESTONE, 1});
+        new BlockMenuPreset(getId(), getItemName()) {
 
-        }
-        @Override
-        public ItemStack getProgressBar() {
-            return new ItemStack(SlimefunItems.ELECTRIC_PRESS);
-        }
-        @Override
-        public String getMachineIdentifier() {return "INGOTCOMP";}
+            @Override
+            public void init() {
+                constructMenu(this);
+            }
 
+            @Override
+            public boolean canOpen(Block b, Player p) {
+                return p.hasPermission("slimefun.inventory.bypass") || Slimefun.getProtectionManager().hasPermission(p, b.getLocation(), Interaction.INTERACT_BLOCK);
+            }
+
+            @Override
+            public int[] getSlotsAccessedByItemTransport(ItemTransportFlow flow) {
+                return new int[0];
+            }
+
+            @Override
+            public int[] getSlotsAccessedByItemTransport(DirtyChestMenu menu, ItemTransportFlow flow, ItemStack item) {
+                if (flow == ItemTransportFlow.WITHDRAW) {
+                    return getOutputSlots();
+                }
+
+                List<Integer> slots = new ArrayList<>();
+
+                for (int slot : getInputSlots()) {
+                    if (SlimefunUtils.isItemSimilar(menu.getItemInSlot(slot), item, true)) {
+                        slots.add(slot);
+                    }
+                }
+
+                if (slots.isEmpty()) {
+                    return getInputSlots();
+                } else {
+                    Collections.sort(slots, compareSlots(menu));
+                    int[] array = new int[slots.size()];
+
+                    for (int i = 0; i < slots.size(); i++) {
+                        array[i] = slots.get(i);
+                    }
+
+                    return array;
+                }
+            }
+        };
     }
+
+    private Comparator<Integer> compareSlots(DirtyChestMenu menu) {
+        return Comparator.comparingInt(slot -> menu.getItemInSlot(slot).getAmount());
+    }
+
+    @Override
+    protected void registerDefaultRecipes() {
+        registerRecipe(45, new ItemStack[] { SlimefunItems.OIL_BUCKET }, new ItemStack[] { new CustomItemStack(SlimefunItems.PLASTIC_SHEET, 8) });
+    }
+
+    @Override
+    public ItemStack getProgressBar() {
+        return new ItemStack(Material.FLINT_AND_STEEL);
+    }
+
+    @Override
+    public String getMachineIdentifier() {
+        return "INGOT_COMP";
+    }
+
+}
