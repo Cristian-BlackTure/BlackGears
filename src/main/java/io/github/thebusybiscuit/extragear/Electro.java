78435c6b085727e2b7ea5cd6741a557c3cbb6b02
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 17:25:49 +0300

    Update Electro.java

--- src/main/java/io/github/thebusybiscuit/extragear/Electro.java
+++ src/main/java/io/github/thebusybiscuit/extragear/Electro.java
@@ -1,1 +1,49 @@
+package io.github.thebusybiscuit.extragear;
+
+import lombok.experimental.UtilityClass;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import io.github.addoncommunity.galactifun.Galactifun;
+import io.github.addoncommunity.galactifun.api.items.Relic;
+import io.github.addoncommunity.galactifun.api.items.spacesuit.SpaceSuit;
+import io.github.addoncommunity.galactifun.api.items.spacesuit.SpaceSuitHelmet;
+import io.github.addoncommunity.galactifun.api.items.spacesuit.SpaceSuitStat;
+import io.github.addoncommunity.galactifun.api.items.spacesuit.SpaceSuitUpgrade;
+import io.github.addoncommunity.galactifun.api.universe.attributes.atmosphere.Gas;
+import io.github.addoncommunity.galactifun.api.worlds.AlienWorld;
+import io.github.addoncommunity.galactifun.base.items.AssemblyTable;
+import io.github.addoncommunity.galactifun.base.items.AtmosphericHarvester;
+import io.github.addoncommunity.galactifun.base.items.AutomaticDoor;
+import io.github.addoncommunity.galactifun.base.items.CircuitPress;
+import io.github.addoncommunity.galactifun.base.items.DiamondAnvil;
+import io.github.addoncommunity.galactifun.base.items.Electrolyzer;
+import io.github.addoncommunity.galactifun.base.items.FusionReactor;
+import io.github.addoncommunity.galactifun.base.items.LaunchPadCore;
+import io.github.addoncommunity.galactifun.base.items.LaunchPadFloor;
+import io.github.addoncommunity.galactifun.base.items.OxygenFiller;
+import io.github.addoncommunity.galactifun.base.items.SpaceSuitUpgrader;
+import io.github.addoncommunity.galactifun.base.items.StargateController;
+import io.github.addoncommunity.galactifun.base.items.StargateRing;
+import io.github.addoncommunity.galactifun.base.items.TechnologicalSalvager;
+import io.github.addoncommunity.galactifun.base.items.knowledge.Observatory;
+import io.github.addoncommunity.galactifun.base.items.knowledge.PlanetaryAnalyzer;
+import io.github.addoncommunity.galactifun.base.items.protection.CoolingUnit;
+import io.github.addoncommunity.galactifun.base.items.protection.ForcefieldGenerator;
+import io.github.addoncommunity.galactifun.base.items.protection.IonDisperser;
+import io.github.addoncommunity.galactifun.base.items.protection.OxygenSealer;
+import io.github.addoncommunity.galactifun.base.items.protection.SpaceHeater;
+import io.github.addoncommunity.galactifun.base.items.rockets.ChemicalRocket;
+import io.github.addoncommunity.galactifun.base.items.rockets.IonRocket;
+import io.github.addoncommunity.galactifun.core.CoreItemGroup;
+import io.github.mooy1.infinitylib.machines.MachineBlock;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineTier;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineType;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.utils.HeadTexture;
+import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
 
