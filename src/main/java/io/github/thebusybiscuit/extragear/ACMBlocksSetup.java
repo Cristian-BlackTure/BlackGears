92a19bead8c0e029d92de1ac6f8288af55a71868
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 20:31:43 +0300

    Update ACMBlocksSetup.java

--- src/main/java/io/github/thebusybiscuit/extragear/ACMBlocksSetup.java
+++ src/main/java/io/github/thebusybiscuit/extragear/ACMBlocksSetup.java
@@ -3,14 +3,10 @@ package io.github.thebusybiscuit.extragear;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.UnplaceableBlock;
-import io.github.jasperchasetoq.compressioncraft.CompressionCraft;
-import io.github.jasperchasetoq.compressioncraft.CompressionCraftItems;
-import io.github.jasperchasetoq.compressioncraft.CompressionCraftRecipeTypes;
-import io.github.jasperchasetoq.compressioncraft.implementation.items.machines.CcItemCompressor;
-import io.github.jasperchasetoq.compressioncraft.implementation.items.machines.CcItemDecompressor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import io.github.thebusybiscuit.extragear;
+import io.github.thebusybiscuit.extragear.IngotComp;
+import io.github.thebusybiscuit.extragear.ACMBlocks;
 
 public class ACMBlocksSetup {
     private ACMBlocksSetup() {}
