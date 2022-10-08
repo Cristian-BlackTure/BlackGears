b079e59586c7c0e0475a06397baf57e4479743c0
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 20:34:36 +0300

    Update IngotComp.java

--- src/main/java/io/github/thebusybiscuit/extragear/IngotComp.java
+++ src/main/java/io/github/thebusybiscuit/extragear/IngotComp.java
@@ -5,7 +5,6 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
-import me.jasperchasetoq.compressioncraft.CompressionCraftItems;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -16,12 +15,8 @@ public class IngotComp extends AContainer implements RecipeDisplayItem {
         @Override
         protected void registerDefaultRecipes() {
 
-            registerRecipe(8, new ItemStack[] {new ItemStack(Material.COBBLESTONE, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_1});
-            registerRecipe(16, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_1, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_2});
-            registerRecipe(24, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_2, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_3});
-            registerRecipe(32, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_3, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_4});
-            registerRecipe(40, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_4, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_5});
-            registerRecipe(48, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_5, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_6});
+            registerRecipe(8, new ItemStack[] {new ItemStack(Material.COBBLESTONE, 8)}, new ItemStack[] {Material.COBBLESTONE, 1});
+            registerRecipe(16, new SlimefunItemStack[]{new SlimefunItemStack(Material.COBBLESTONE, 8)}, new ItemStack[] {Material.COBBLESTONE, 1});
 
         }
         @Override
