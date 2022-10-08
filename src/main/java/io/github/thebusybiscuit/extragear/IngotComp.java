11a600d58d03d092096381727bd47c29233a2d3e
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 20:00:54 +0300

    Create IngotComp.java

---
+++ src/main/java/io/github/thebusybiscuit/extragear/IngotComp.java
@@ -0,0 +1,38 @@
+package io.github.thebusybiscuit.extragear;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import me.jasperchasetoq.compressioncraft.CompressionCraftItems;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+public class CcItemCompressor extends AContainer implements RecipeDisplayItem {
+    @ParametersAreNonnullByDefault
+    public CcItemCompressor(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+     super(itemGroup, item, recipeType, recipe);}
+        @Override
+        protected void registerDefaultRecipes() {
+
+            registerRecipe(8, new ItemStack[] {new ItemStack(Material.COBBLESTONE, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_1});
+            registerRecipe(16, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_1, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_2});
+            registerRecipe(24, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_2, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_3});
+            registerRecipe(32, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_3, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_4});
+            registerRecipe(40, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_4, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_5});
+            registerRecipe(48, new SlimefunItemStack[]{new SlimefunItemStack(CompressionCraftItems.JC_COBBLESTONE_5, 8)}, new ItemStack[] {CompressionCraftItems.JC_COBBLESTONE_6});
+
+        }
+        @Override
+        public ItemStack getProgressBar() {
+            return new ItemStack(SlimefunItems.ELECTRIC_PRESS);
+        }
+        @Override
+        public String getMachineIdentifier() {return "JC_COMPRESSOR";}
+
+    }
+
